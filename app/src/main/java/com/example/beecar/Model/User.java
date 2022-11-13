b24968b2c2c41e68113f527a7fc6e186ba648c6d
admin <minhnqph25450@fpt.edu.vn>
2022-11-13 22:19:43 +0700

    database

--- app/src/main/java/com/example/beecar/Model/User.java
+++ app/src/main/java/com/example/beecar/Model/User.java
@@ -1,4 +1,52 @@
 package com.example.beecar.Model;
 
 public class User {
+    int id;
+    String user_name;
+    String password;
+    String full_name;
+    int position;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
 }
