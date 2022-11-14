363d27b76f4f57086512c715772d8a282bad0fbb
admin <minhnqph25450@fpt.edu.vn>
2022-11-14 23:53:57 +0700

    animation

--- app/src/main/java/com/example/beecar/SplashActivity.java
+++ app/src/main/java/com/example/beecar/SplashActivity.java
@@ -2,10 +2,14 @@ package com.example.beecar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,6 +24,14 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
            imageView = findViewById(R.id.imageView);
            tvSplash = findViewById(R.id.tvSplash);
+        Animation animationImg = AnimationUtils.loadAnimation(this,R.anim.anim_logo_splash_screen);
+        imageView.startAnimation(animationImg);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(tvSplash,"translationY",-100f,0f);
+        animator.setDuration(2000);
+        animator.setRepeatCount(0);
+        animator.start();
+        Animation animationTitle = AnimationUtils.loadAnimation(this,R.anim.anim_title_splash_screen);
+            tvSplash.startAnimation(animationTitle);
            Typeface font = Typeface.createFromAsset(getAssets(),"fonts/FredokaOne-Regular.ttf");
            tvSplash.setTypeface(font);
 
