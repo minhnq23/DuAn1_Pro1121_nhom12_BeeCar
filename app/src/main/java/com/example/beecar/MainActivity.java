450efd6db77c90c9ada1cebfae1dd724751ef676
admin <minhnqph25450@fpt.edu.vn>
2022-11-16 20:26:14 +0700

    register and login gd1

--- app/src/main/java/com/example/beecar/MainActivity.java
+++ app/src/main/java/com/example/beecar/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.beecar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.beecar.Database.MyDbHelper;
 
 public class MainActivity extends AppCompatActivity {
 //    MyDbHelper myDbHelper;
+    TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,22 @@ public class MainActivity extends AppCompatActivity {
 //        myDbHelper.getReadableDatabase();
         Intent intent = new Intent(this,SplashActivity.class);
         startActivity(intent);
+        tvRegister = findViewById(R.id.tv_register);
+        tvRegister.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction()== MotionEvent.ACTION_DOWN){
+                    tvRegister.setTextColor(Color.BLUE);
+                }
+                if (motionEvent.getAction()== MotionEvent.ACTION_UP){
+                    tvRegister.setTextColor(Color.WHITE);
+                }
+                return false;
+            }
+        });
+        tvRegister.setOnClickListener(view -> {
+            startActivity(new Intent(this,PositionActivity.class));
+        });
+
     }
 }
