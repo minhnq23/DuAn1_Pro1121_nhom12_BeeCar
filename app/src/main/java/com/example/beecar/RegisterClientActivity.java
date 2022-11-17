5bc1d8773ac1be6dc163c1dca82de7ba31fc96f6
admin <minhnqph25450@fpt.edu.vn>
2022-11-17 15:20:26 +0700

    register client

--- app/src/main/java/com/example/beecar/RegisterClientActivity.java
+++ app/src/main/java/com/example/beecar/RegisterClientActivity.java
@@ -1,14 +1,30 @@
 package com.example.beecar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class RegisterClientActivity extends AppCompatActivity {
-
+    Toolbar toolbar ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_client);
+        toolbar = findViewById(R.id.tool_bar_register_client);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_orange);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
