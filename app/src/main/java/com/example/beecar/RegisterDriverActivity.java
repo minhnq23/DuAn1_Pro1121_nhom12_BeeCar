f40d6941c8a27a9113dc579035f3ce3c7fa9cc9b
admin <minhnqph25450@fpt.edu.vn>
2022-11-17 14:56:16 +0700

    add btn on back activity

--- app/src/main/java/com/example/beecar/RegisterDriverActivity.java
+++ app/src/main/java/com/example/beecar/RegisterDriverActivity.java
@@ -1,9 +1,11 @@
 package com.example.beecar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class RegisterDriverActivity extends AppCompatActivity {
     Toolbar toolbar ;
@@ -17,5 +19,13 @@ public class RegisterDriverActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 //        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_orange);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
