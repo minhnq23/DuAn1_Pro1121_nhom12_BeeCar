d910d2066553ff8b4f16bdb35a6d5cc3aaa4116f
khanhnvph 2 6 6 6 9 fplhn <khanhnvph26669@fpt.edu.vn>
2022-11-18 14:51:37 +0700

    suadao

--- app/src/main/java/com/example/beecar/DAO/DriverDAO.java
+++ app/src/main/java/com/example/beecar/DAO/DriverDAO.java
@@ -8,70 +8,56 @@ import android.database.sqlite.SQLiteDatabase;
 import com.example.beecar.Database.MyDbHelper;
 import com.example.beecar.Model.Driver;
 import com.example.beecar.Model.User;
-import com.example.beecar.Model.Vehicles;
 
 import java.util.ArrayList;
 
 public class DriverDAO {
-
-    MyDbHelper myDbHelper;
-
+    private SQLiteDatabase database;
+    private MyDbHelper dbHelper;
+    public static final String TABLE_NAME = "tb_driver";
 
     public DriverDAO(Context context) {
-        myDbHelper = new MyDbHelper(context);
-        myDbHelper.getWritableDatabase();
+        dbHelper = new MyDbHelper(context);
+        database = dbHelper.getWritableDatabase();
     }
 
     public ArrayList<Driver> selectAll(){
-        ArrayList<Driver> list = new ArrayList<>();
-        SQLiteDatabase db = myDbHelper.getReadableDatabase();
-        String sql = "SELECT*FROM tb_driver";
-        Cursor cursor = db.rawQuery(sql,null);
+        ArrayList<Driver> list_driver = new ArrayList<>();
+        String select = "SELECT * FROM "+ DriverDAO.TABLE_NAME;
+        Cursor cursor  = database.rawQuery(select, null);
         if (cursor.moveToFirst()){
             while (!cursor.isAfterLast()){
-                Driver objD = new Driver();
-                objD.setId(cursor.getInt(0));
-                objD.setUser_name(cursor.getString(1));
-                objD.setPassword(cursor.getString(2));
-                objD.setFull_name(cursor.getString(3));
-                objD.setLuongcb(cursor.getInt(4));
-                objD.setStatus_driver(cursor.getInt(5));
-                objD.setUser_id(cursor.getInt(6));
-
-                list.add(objD);
+                Driver driver = new Driver();
+                driver.setId(cursor.getInt(0));
+                driver.setUser_name(cursor.getString(1));
+                driver.setPassword(cursor.getString(2));
+                driver.setFull_name(cursor.getString(3));
+                driver.setStatus_driver(cursor.getInt(4));
+             //   User.setPosition(cursor.getString(4));
+                list_driver.add(driver);
                 cursor.moveToNext();
-
             }
+            cursor.close();
         }
-        cursor.close();
-        return list;
+        return list_driver;
     }
 
-    public boolean insert(Driver objD){
-        SQLiteDatabase db = myDbHelper.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(Driver.COL_user_name,objD.getUser_name());
-        values.put(Driver.COL_password,objD.getPassword());
-        values.put(Driver.COL_full_name,objD.getFull_name());
-        values.put(Driver.COL_luongcb,objD.getLuongcb());
-        values.put(Driver.COL_status,objD.getStatus_driver());
-        values.put(Driver.COL_user_id,objD.getUser_id());
-        long row = db.insert(Driver.TB_Name,null,values);
-        return row>0;
+    public long insertDriver(Driver driver){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("idDriver",driver.getId());
+        contentValues.put("user_name",driver.getUser_name());
+        contentValues.put("password",driver.getPassword());
+        contentValues.put("full_name",driver.getFull_name());
+        contentValues.put("status_driver",driver.getStatus_driver());
+        try {
+            long res = database.insert(TABLE_NAME,null,contentValues);
+            if(res<0){
+                return -1;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return 1;
     }
-    public boolean update(Driver objD){
-        SQLiteDatabase db = myDbHelper.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(Driver.COL_user_name,objD.getUser_name());
-        values.put(Driver.COL_password,objD.getPassword());
-        values.put(Driver.COL_full_name,objD.getFull_name());
-        values.put(Driver.COL_luongcb,objD.getLuongcb());
-        values.put(Driver.COL_status,objD.getStatus_driver());
-        values.put(Driver.COL_user_id,objD.getUser_id());
-        int row = db.update(Driver.TB_Name,values,"id=?", new String[]{objD.getId()+""});
-        return row>0;
-    }
-
-
 
 }
