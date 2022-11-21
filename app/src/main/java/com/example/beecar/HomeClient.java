2a26b59cba95301041f9a7457a7cbf7b929fab09
ASUS <thaipqph13461@fpt.edu.vn>
2022-11-21 10:41:14 +0700

    fixhomeclient3

--- app/src/main/java/com/example/beecar/HomeClient.java
+++ app/src/main/java/com/example/beecar/HomeClient.java
@@ -10,6 +10,6 @@ public class HomeClient extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_client);
+        setContentView(R.layout.activity_home_client2);
     }
 }
